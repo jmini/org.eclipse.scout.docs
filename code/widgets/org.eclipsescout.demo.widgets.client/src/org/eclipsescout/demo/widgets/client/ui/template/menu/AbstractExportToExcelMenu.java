ebb573cd8429cda33a018a2bfa33da8eefc4b660
Jeremie Bresson <jbr@bsiag.com>
2015-05-28 05:25:53 +0200

    fix AbstractExportToExcelMenu and target (3.10 Branch)

--- code/widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/template/menu/AbstractExportToExcelMenu.java
+++ code/widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/template/menu/AbstractExportToExcelMenu.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
 package org.eclipsescout.demo.widgets.client.ui.template.menu;
 
 import java.io.File;
@@ -5,6 +15,7 @@ import java.io.File;
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.rt.client.ui.action.menu.AbstractMenu;
 import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.docx4j.client.ScoutXlsxSpreadsheetAdapter;
 import org.eclipse.scout.rt.shared.TEXTS;
 import org.eclipse.scout.rt.shared.services.common.shell.IShellService;
 import org.eclipse.scout.service.SERVICES;
